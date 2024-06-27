2e32004cba465d124ca041950cbbf510544cb807
CodeMaverick2 <codemaverick2@gmail.com>
2024-06-27 16:35:04 +0530

    Cp code

--- temp/temp.java
+++ temp/temp.java
@@ -10,27 +10,33 @@ class Exchange{
 
 public class temp {
     public static void main(String[] args) {
-        int B = 3;
-        int[] A = {1, 3, -1, -3, 5, 3, 6, 7};
+        System.out.println("Hello, World!");
+        Thread currentThread = Thread.currentThread();
+        System.out.println("Thread Name: " + currentThread.getName());
+        System.out.println("Thread ID: " + currentThread.getId());
+        System.out.println("Thread Priority: " + currentThread.getPriority());
+        System.out.println("Thread State: " + currentThread.getState());
+        // int B = 3;
+        // int[] A = {1, 3, -1, -3, 5, 3, 6, 7};
 
-        Deque<Integer> q = new ArrayDeque<>();
-        int[] ans = new int[A.length-B+1];
-        int k = 0;
-        for(int i=0;i<A.length;i++){
-            if(!q.isEmpty() && q.peek()==i-B){
-                q.pop();
-            }
-            while(!q.isEmpty() && A[q.peekLast()]<=A[i]){
-                q.pollLast();
-            }
-            q.add(i);
-            if(i>=B-1){
-                ans[k++] = A[q.peek()];
-            }
-        }
-        for(int i=0;i<ans.length;i++){
-            System.out.println(ans[i]);
-        }
+        // Deque<Integer> q = new ArrayDeque<>();
+        // int[] ans = new int[A.length-B+1];
+        // int k = 0;
+        // for(int i=0;i<A.length;i++){
+        //     if(!q.isEmpty() && q.peek()==i-B){
+        //         q.pop();
+        //     }
+        //     while(!q.isEmpty() && A[q.peekLast()]<=A[i]){
+        //         q.pollLast();
+        //     }
+        //     q.add(i);
+        //     if(i>=B-1){
+        //         ans[k++] = A[q.peek()];
+        //     }
+        // }
+        // for(int i=0;i<ans.length;i++){
+        //     System.out.println(ans[i]);
+        // }
         // int A = 5;
         // ArrayList<Integer> ans = new ArrayList<>();
         // Queue<Integer> q = new LinkedList<>();
